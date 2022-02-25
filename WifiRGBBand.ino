94ddc7784b9337d55580f427bceade58af91e399
Friedinger <71267611+Friedinger@users.noreply.github.com>
2022-02-25 16:33:33 +0100

    Removed comments

--- WifiRGBBand.ino
+++ WifiRGBBand.ino
@@ -57,14 +57,6 @@ const char html[] = R"(
 </body>
 )";
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(100, pin, NEO_GRB + NEO_KHZ800);
 
 WiFiServer server(80);
